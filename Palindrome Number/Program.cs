597c711a70e6370834738046e17f7eb691c2550c
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-12 01:41:30 +0530

    Add project files.

---
+++ Palindrome Number/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // X = 121;
+            
+            int []a = { 1, 2, 1 };
+            int temp = 0;
+            int temp2 = 0;
+            for(int j =0;j<a.Length;j++)
+            {
+                temp2 = a[j];
+            }
+            for(int i =a.Length-1;i>=0;i--)
+            {
+                temp = a[i];
+            }
+            if(temp==temp2)
+            {
+                Console.WriteLine("This is palindrome number");
+            }
+            else
+            {
+                Console.WriteLine("This is not palindrome");
+            }
+            
+            {
+
+            }
+        }
+    }
+}
